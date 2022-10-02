3868da1d695c1571e2008b512ef0fe96c7e138ab
Mykyta Storozhenko <mst387@uky.edu>
2022-10-02 11:37:23 -0400

    Fix http to https in location-getter module

--- src/scripts/location-getter.js
+++ src/scripts/location-getter.js
@@ -1,5 +1,5 @@
 const getLocation = async function(town){
-    const locRes = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${town}&limit=3&appid=885307ed8d8bed7cfb18a647ea6b3322`);
+    const locRes = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${town}&limit=3&appid=885307ed8d8bed7cfb18a647ea6b3322`);
     const data = await locRes.json();
     return data;
 };
